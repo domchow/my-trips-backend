d69717e767d2e8eee00b2472ad53a6c931d3edd5
domchow <dch@hellofresh.com>
2021-03-14 14:56:13 +0100

    Fix auth

--- src/main/java/com/my/trips/security/SecurityConfig.java
+++ src/main/java/com/my/trips/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -24,15 +25,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/home").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .httpBasic();
-
-        http.formLogin()
-                .loginPage("/login").permitAll()
+                .httpBasic()
                 .and()
-                .logout().permitAll();
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Bean
